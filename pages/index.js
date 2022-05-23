b0ee31f261778ba3897543b7490261ac98430fa7
henriquesbarros19 <henrique.barros@compasso.com.br>
2022-05-22 23:14:05 -0300

    feat: adding config and inital use sanity cms

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData}) => {
   return (
-    <>
-      <HeroBanner />
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className="products-heading">
         <h2>Best Selling oducts</h2>
@@ -12,12 +13,24 @@ const Home = () => {
       </div>
 
       <div className="products-container">
-        {['Product 1', 'Product 2'].map(product => product)}
+        {products?.map(product => product.name)}
       </div>
 
       <FooterBanner />
-    </>
+    </div>
   )
 }
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+};
